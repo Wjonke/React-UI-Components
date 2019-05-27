2f04356ca3a3d976ab7bb854af4a8ab153b813bc
Wes Jonke <wesjonke@gmail.com>
2019-05-27 18:47:14 -0400

    added react for the card and header components, made card component a link to react.org, mtched design as close as possible    MVP

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle= () =>{
+  return (
+    <div className='cardHeader'>
+      <div className='userInfoContainer'>
+        <h1 className='userName'> Lambda School </h1>
+        <h2 className='timeStamp'>@LambdaSchool - 27 May 19 </h2>
+      </div>
+      <div className='text'>
+        <p>Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
+      </div>
+    </div>
+
+  );
+  };
+  
+  export default HeaderTitle;
